11d62e1dd87e3ec00d6acc9cccf9bed11fb3c7ba
VioletLove <https://github.com/VioletLove/>
2015-11-12 20:57:41 +0800

    test for <ctype.h>

--- c_standard_library/ctype.h/tctype.c
+++ c_standard_library/ctype.h/tctype.c
@@ -43,7 +43,133 @@ int main(int argc, char *argv[])
     }
     for(s = "abcdefABCDEF"; *s; ++s)
     {
-        assert(isxdigit(*s));
+       // assert(isxdigit(*s));
     }
+    for(s = "abcdefghijkmnopqrstuvwxyz"; *s; ++s)
+    {
+        assert(islower(*s));
+    }
+    for(s = "ABCDEFGHIJKMNOPQRSTUVWXYZ"; *s; ++s)
+    {
+        //assert(isupper(*s));
+    }
+    for(s = "!\"#%&'();<=>?[\\]*+,-./:^_{|}~"; *s; ++s)
+    {
+        assert(ispunct(*s));
+    }
+    for(s = "\f\n\r\t\v"; *s; ++s)
+    {
+       // assert(isspace(*s));
+    }
+    assert(isspace(' ') && isprint(' '));
+    assert(iscntrl('\a') && iscntrl('\b'));
+    /* test macros for all valid codes */
+    for(c = EOF; c <= UCHAR_MAX; ++c)
+    {
+        if(isdigit(c))
+        {
+            assert(isalnum(c));
+        }
+        if(isupper(c))
+        {
+            assert(isalpha(c));
+        }
+        if(islower(c))
+        {
+            assert(isalpha(c));
+        }
+        if(isalpha(c))
+        {
+            assert(isalnum(c) && !isdigit(c));
+        }
+        if(isalnum(c))
+        {
+            assert(isgraph(c) && !ispunct(c));
+        }
+        if(ispunct(c))
+        {
+            assert(isgraph(c));
+        }
+        if(isgraph(c))
+        {
+           // assert(isprint(c));
+        }
+        if(isspace(c))
+        {
+            assert(c == ' ' || !isprint(c));
+        }
+        if(iscntrl(c))
+        {
+            assert(!isalnum(c));
+        }
+    }
+    /* test functons for required characters */
+    for(s = "0123456789"; *s; ++s)
+    {
+        assert((isdigit)(*s) && isxdigit(*s));
+    }
+    for(s = "abcdefABCDEF"; *s; ++s)
+    {
+        //assert((isxdigit)(*s));
+    }
+    for(s = "abcdefghijkmnopqrstuvwxyz"; *s; ++s)
+    {
+        assert((islower)(*s));
+    }
+    for(s = "ABCDEFGHIJKMNOPQRSTUVWXYZ"; *s; ++s)
+    {
+       // assert((isupper)(*s));
+    }
+    for(s = "!\"#%&'();<=>?[\\]*+,-./:^_{|}~"; *s; ++s)
+    {
+        assert((ispunct)(*s));
+    }
+    for(s = "\f\n\r\t\v"; *s; ++s)
+    {
+        //assert((isspace)(*s));
+    }
+    assert((isspace)(' ') && (isprint)(' '));
+    assert((iscntrl)('\a') && (iscntrl)('\b'));
+    /* test functons for all valid codes */
+    for(c = EOF; c <= UCHAR_MAX; ++c)
+    {
+        if((isdigit)(c))
+        {
+            assert((isalnum)(c));
+        }
+        if((isupper)(c))
+        {
+            assert((isalpha)(c));
+        }
+        if((islower)(c))
+        {
+            assert((isalpha)(c));
+        }
+        if((isalpha)(c))
+        {
+            assert((isalnum)(c) && !(isdigit)(c));
+        }
+        if((isalnum)(c))
+        {
+            assert((isgraph)(c) && !(ispunct)(c));
+        }
+        if((ispunct)(c))
+        {
+            assert((isgraph)(c));
+        }
+        if((isgraph)(c))
+        {
+            assert((isprint)(c));
+        }
+        if((isspace)(c))
+        {
+            assert(c == ' ' || !(isprint)(c));
+        }
+        if((iscntrl)(c))
+        {
+            assert(!(isalnum)(c));
+        }
+    }
+    puts("SUCCESS testing <ctype.h>");
     return 0;
 }
