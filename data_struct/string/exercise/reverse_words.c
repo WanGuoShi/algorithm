4b58df65481ff34e6f060494c2a54b82d4c37443
VioletLove <1182248598@qq.com>
2016-01-17 14:44:27 +0800

    单词翻转，用指针数组

---
+++ data_struct/string/exercise/reverse_words.c
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: reverse_words.c
+	> Author: daemonghost
+	> Mail: yuanxingnepu@gmail.com
+	> Created Time: 2016年01月17日 星期日 14时05分25秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+
+void ReverseWords(char *(s[]), int from, int to){
+    char *tmp;
+    while(from < to){
+        tmp = s[from];
+        s[from++] = s[to];
+        s[to--] = tmp;
+    }
+}
+
+int main()
+{
+    char *(s[]) = {"I","am","a","student."};
+    ReverseWords(s, 0, 3);
+    for(int i = 0; i < 4; i++){
+        puts(s[i]);
+    }
+    return 0;
+}
+
