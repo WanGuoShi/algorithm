d358609d914b2900cc0a8cb072949957e1ebcd6a
VioletLove <1182248598@qq.com>
2016-01-21 16:17:51 +0800

    全排列，字典序解法

---
+++ data_struct/string/permutation.c
@@ -0,0 +1,45 @@
+/*************************************************************************
+	> File Name: permutation.c
+	> Author: daemonghost
+	> Mail: yuanxingnepu@gmail.com
+	> Created Time: 2016年01月20日 星期三 15时35分26秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+void swap(char *a, char *b);
+void permutation(char *s, int from, int to);
+
+int main()
+{
+    char s[] = "abc";
+    permutation(s, 0, 2);
+    return 0;
+}
+
+void permutation(char *s, int from, int to){
+    int i, j;
+    if(to <= 1){
+        return;
+    }
+
+    if(from == to){
+        for(i = 0; i <= to; i++){
+            printf("%c",s[i]);
+        }
+        printf("\n");
+    }else{
+        for(j = from; j <= to; j++){
+            swap(&s[j], &s[from]);
+            permutation(s, from + 1, to);
+            swap(&s[j], &s[from]);
+        }
+    }
+}
+
+void swap(char *a, char *b){
+    char t;
+    t = *a;
+    *a = *b;
+    *b = t;
+}
